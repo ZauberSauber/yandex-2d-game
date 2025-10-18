5f3dedeef226dd02ab0678a15378cfe856c490e9
Torzhevskiy Iliya <Iliya.torzhevskiy@mail.ru>
2025-10-18 22:05:12 +0700

    add MockData

---
+++ packages/client/src/pages/profile/MockData.ts
@@ -0,0 +1,44 @@
+export const userMockData = {
+  avatar: '',
+  displayName: 'Neo_Tokyo_User',
+  firstName: 'Neo',
+  secondName: 'Tokyo',
+  login: 'Neo_Tokyo_User',
+  email: 'user@neotokyo.net',
+  phone: '+70000000000',
+  biography: 'Хакер из будущего. Специализация: нейронные сети и кибербезопасность.',
+  level: 42,
+  rating: 1850,
+  statistics: [
+    {
+      name: 'message',
+      value: 156,
+    },
+    {
+      name: 'message',
+      value: 156,
+    },
+    {
+      name: 'activity',
+      value: 89,
+    },
+    {
+      name: 'firstActivity',
+      value: '17.10.2020',
+    },
+  ],
+  achivments: [
+    {
+      name: 'theFirstHack',
+    },
+    {
+      name: 'activeParticipant',
+    },
+    {
+      name: 'top10',
+    },
+    {
+      name: 'speed',
+    },
+  ],
+};
--- packages/client/src/pages/profile/ProfilePage.tsx
+++ packages/client/src/pages/profile/ProfilePage.tsx
@@ -1,3 +1,17 @@
-export function ProfilePage() {
-  return <>Profile Page</>;
-}
+import { Typography } from 'antd';
+
+import style from './ProfilePage.module.scss';
+
+export const ProfilePage = () => {
+  const { Text } = Typography;
+  return (
+    <div className={style.container}>
+      Profile Page123
+      <div>
+        <Text> avatar</Text>
+
+        <div></div>
+      </div>
+    </div>
+  );
+};
